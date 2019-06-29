40d16d8686bed9d339b261a11f386d5cae3ef1db
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-06-29 11:53:26 +0530

    add more tests

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -2,6 +2,7 @@ import pytest
 from requests.exceptions import MissingSchema
 
 from fetch import fetch
+from fetch.helpers import sanitize_link
 
 
 def test_fetch():
@@ -15,3 +16,15 @@ def test_fetch_with_invalid_url():
     url = 'foo'
     with pytest.raises(MissingSchema):
         fetch(url)
+
+
+def test_sanitize_if_link_starts_with_slash():
+    url = 'http://websiteopedia.com'
+    link = '/about'
+    assert sanitize_link(link, url) == 'http://websiteopedia.com/about'
+
+
+def test_sanitize_if_link_starts_with_double_slash():
+    url = 'http://websiteopedia.com'
+    link = '//instagram.com'
+    assert sanitize_link(link, url) == 'http://instagram.com'
