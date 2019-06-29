ae3e82d58bf9d14215052addc0b5d85adae55b1e
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-06-29 11:42:05 +0530

    add info to README.md and pytest

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# fetch
+# Welcome to fetch 👋
+
+> Get images and hyperlinks of any web page
+
+## Install
+
+Make you have installed Python 3.6+ and PIP installed.
+
+```sh
+make
+```
+
+## Usage
+
+```python
+url = 'https://websiteopedia.com/instagram.com'
+fetch(url)
+# returns {'assets': [], link: []}
+```
+
+## Run tests
+
+```sh
+make test
+```
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,9 +1,16 @@
 import pytest
+from requests.exceptions import MissingSchema
 
 from fetch import fetch
+
 
 def test_fetch():
     url = 'https://websiteopedia.com/instagram.com'
     links = fetch(url)
     assert len(links['assets']) == 1
     assert len(links['links']) > 1
+
+def test_fetch_with_invalid_url():
+    url = 'foo'
+    with pytest.raises(MissingSchema):
+        links = fetch(url)
