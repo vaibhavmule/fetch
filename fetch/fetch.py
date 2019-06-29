f9f0a0a6ae88bbed88afbedfe5bf7542d2dbaf09
Vaibhav Mule <vaibhavmule135@gmail.com>
2019-06-29 09:48:17 +0530

    add fetch file

---
+++ fetch/fetch.py
@@ -0,0 +1,11 @@
+import requests
+
+def fetch(url):
+    """Gets images links and hyperlinks of specified webpage.
+
+    :param url: The url of the webpage that needs to scraped.
+    :type url: str
+    :returns: a dict containing list of image links and hyperlinks
+    :rtype: dict
+    """
+    pass
